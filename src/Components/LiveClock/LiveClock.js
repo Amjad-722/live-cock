1af332e00e727e3c725dcdeddbf27420a70bdc3c
Amjad-722 <aliamjad722ee@gmail.com>
2024-07-24 16:47:18 +0500

    changes

--- src/Components/LiveClock/LiveClock.js
+++ src/Components/LiveClock/LiveClock.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import moment from 'moment-timezone';
+import React, { useState, useEffect } from "react";
+import moment from "moment-timezone";
 
 const LiveClock = ({ timeZone }) => {
-  const [time, setTime] = useState(moment.tz(timeZone).format('HH:mm:ss'));
+  const [time, setTime] = useState(moment.tz(timeZone).format("HH:mm:ss"));
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setTime(moment.tz(timeZone).format('HH:mm:ss'));
+      setTime(moment.tz(timeZone).format("HH:mm:ss"));
     }, 1000);
 
     return () => clearInterval(intervalId);
