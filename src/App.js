d0898da58422612ca39b8304da74f30eb3640b7d
Amjad-722 <aliamjad722ee@gmail.com>
2024-07-24 17:10:18 +0500

    changs in p

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const App = () => {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
     <div className="flex flex-col justify-center items-center border-spacing-4 pt-48 shadow-xl shadow-red-700 bg-white p-28 rounded-lg">
       <h1 className="font-extrabold text-3xl  text-gray-700 mb-4 ">Live Clock</h1>
-      <p className="mb-2">Current Time in New York:</p>
+      <p className="mb-4">Current Time in New York:</p>
       <LiveClock timeZone="America/New_York" />
-      <p className="mt-4 mb-2">Current Time in London:</p>
+      <p className="mt-6 mb-2">Current Time in London:</p>
       <LiveClock timeZone="Europe/London" />
       <p className="mt-4 mb-2">Current Time in Tokyo:</p>
       <LiveClock timeZone="Asia/Tokyo" />
