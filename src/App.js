fe769c6a0c3b2cbb7d125bada755d52940490d0e
Amjad-722 <aliamjad722ee@gmail.com>
2024-07-25 16:36:56 +0500

    added taiwlind to the liveclock comp

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import LiveClock from './Components/LiveClock/LiveClock';
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-    <div className="flex flex-col justify-center items-center border-spacing-4 pt-48 shadow-xl shadow-red-700 bg-white p-28 rounded-lg">
+    <div className="flex justify-center items-center min-h-screen bg-slate-700">
+    <div className="flex flex-col justify-center items-center border-spacing-4 pt-48 shadow-xl shadow-red-700 bg-zinc-500   p-28 rounded-lg">
  
       
       
-      <h1 className="font-extrabold text-3xl  text-gray-700 mb-4 ">Live Clock</h1>
+      <h1 className="font-extrabold text-3xl  text-zinc-100 mb-4 ">Live Clock</h1>
       
-      <p className="mt-6  font-medium mb-2">Current Time in London:</p>
+      <p className="mt-6  text-white font-medium text-lg mb-2">Current Time in London:</p>
  
       <LiveClock timeZone="Europe/London" />
-      <p className="mt-4 font-medium mb-2">Current Time in Tokyo:</p>
+      <p className="mt-4  text-white  text-lg  font-medium mb-2">Current Time in Tokyo:</p>
       <LiveClock timeZone="Asia/Tokyo" />
     </div>
   </div>
