e89234d1ff4cc76bbcd68967c6343f090a8c6263
Amjad-722 <aliamjad722ee@gmail.com>
2024-07-25 16:09:48 +0500

    added mt 3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
     <div className="flex flex-col justify-center items-center border-spacing-4 pt-48 shadow-xl shadow-red-700 bg-white p-28 rounded-lg">
-      <h1 className="font-extrabold text-3xl  text-gray-700 mb-4 ">Live Clock</h1>
+      <h1 className="font-extrabold text-3xl  text-gray-700 mb-3 ">Live Clock</h1>
       <p className="mb-4">Current Time in New York:</p>
       <LiveClock timeZone="America/New_York" />
       <p className="mt-6 mb-2">Current Time in London:</p>
